21f1feef30d391b4d438baaa54edbd89e34d1859
Jianqing.Peng <Jianqing.Peng@ef.com>
2014-06-19 10:34:27 +0800

    init

---
+++ app/src/main/java/me/pjq/objectpool/app/ObjectPoolMain.java
@@ -0,0 +1,23 @@
+package me.pjq.objectpool.app;
+
+import java.sql.Connection;
+
+/**
+ * Created by pengjianqing on 6/19/14.
+ */
+public class ObjectPoolMain {
+
+    public static void main(String []args){
+        JDBCConnectionPool pool = new JDBCConnectionPool(
+                "org.hsqldb.jdbcDriver", "jdbc:hsqldb://localhost/mydb",
+                "sa", "secret");
+
+        //Get a connection
+        Connection connection = pool.checkOut();
+
+
+        //return the connection
+        pool.checkIn(connection);
+
+    }
+}
